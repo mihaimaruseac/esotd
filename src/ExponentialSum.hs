12f35f5f31756e90f15c52b23631bec9e3225dbc
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-05-14 21:21:09 -0700

    Expand dcoumentation with usage example

--- src/ExponentialSum.hs
+++ src/ExponentialSum.hs
@@ -6,6 +6,14 @@ module ExponentialSum
   ) where
 
 -- | Compute the value of the nth term, given the initial coefficients
+--
+-- For example, here is the third term computed where the coefficients are
+-- current date:
+--
+-- >>> term [12, 5, 2018] 3
+-- 2.063379583746283
+-- >>> term [12, 5, 18] 3
+-- 3.55
 term :: [Double] -> Double -> Double
 term xs n = sum $ zipWith sumTerm xs [1..]
   where
