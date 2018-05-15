69f240c2aec9e6273976809d005210d12e40ce8e
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-05-14 21:21:09 -0700

    Compute one term of the sum

--- src/ExponentialSum.hs
+++ src/ExponentialSum.hs
@@ -2,5 +2,12 @@
 module ExponentialSum
   (
     -- * Exported functions
+    term
   ) where
 
+-- | Compute the value of the nth term, given the initial coefficients
+term :: [Double] -> Double -> Double
+term xs n = sum $ zipWith sumTerm xs [1..]
+  where
+    sumTerm :: Double -> Int -> Double
+    sumTerm x i = n ^ i / x
