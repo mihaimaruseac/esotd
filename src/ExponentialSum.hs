30a0adf9073206309bf58c7283eef7d8c173e2e4
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-05-14 21:22:50 -0700

    Expand dcoumentation with a few properties

--- src/ExponentialSum.hs
+++ src/ExponentialSum.hs
@@ -14,6 +14,22 @@ module ExponentialSum
 -- 2.063379583746283
 -- >>> term [12, 5, 18] 3
 -- 3.55
+--
+-- There are some properties which might be useful:
+--
+-- 1. If the coefficients are missing, we want to return a 0 for any term
+-- index:
+--
+-- prop> term [] x == 0
+--
+-- 2. If the term is 0, we output 0 regardless of the coefficients, provided
+-- at least one is non-zero
+--
+-- prop> a == 0 || term (a:xs) 0 == 0
+--
+-- There might be a few more properties but we can ignore them for now, as
+-- some might fail due to numerical inaccuracies. We are also sidestepping
+-- NaNs and infinities at the moment.
 term :: [Double] -> Double -> Double
 term xs n = sum $ zipWith sumTerm xs [1..]
   where
