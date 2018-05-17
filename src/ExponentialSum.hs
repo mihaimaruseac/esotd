5da38308cecb1dc7fc626dc6d31be5ba1171d6bc
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-05-16 21:32:39 -0700

    Alpha renames

--- src/ExponentialSum.hs
+++ src/ExponentialSum.hs
@@ -7,33 +7,33 @@ Compute the exponential sum of the day, to be plotted later.
 
 module ExponentialSum where
 
--- | Compute the value of the nth term, given the initial coefficients
+-- | Compute the value of the real argument of the exponential, at point n,
+-- given the initial coefficients.
 --
--- For example, here is the third term computed where the coefficients are
+-- For example, here is the third value computed where the coefficients are
 -- current date:
 --
--- >>> term [12, 5, 2018] 3
+-- >>> realArg [12, 5, 2018] 3
 -- 2.063379583746283
--- >>> term [12, 5, 18] 3
+-- >>> realArg [12, 5, 18] 3
 -- 3.55
 --
 -- There are some properties which might be useful:
 --
--- 1. If the coefficients are missing, we want to return a 0 for any term
--- index:
+-- 1. If the coefficients are missing, we want to return a 0 for any index:
 --
--- prop> term [] x == 0
+-- prop> realArg [] x == 0
 --
--- 2. If the term is 0, we output 0 regardless of the coefficients, provided
+-- 2. If the index is 0, we output 0 regardless of the coefficients, provided
 -- at least one is non-zero
 --
--- prop> a == 0 || term (a:xs) 0 == 0
+-- prop> a == 0 || realArg (a:xs) 0 == 0
 --
 -- There might be a few more properties but we can ignore them for now, as
 -- some might fail due to numerical inaccuracies. We are also sidestepping
 -- NaNs and infinities at the moment.
-term :: [Double] -> Double -> Double
-term xs n = sum $ zipWith sumTerm xs [1..]
+realArg :: [Double] -> Double -> Double
+realArg xs n = sum $ zipWith power xs [1..]
   where
-    sumTerm :: Double -> Int -> Double
-    sumTerm x i = n ^ i / x
+    power :: Double -> Int -> Double
+    power x i = n ^ i / x
