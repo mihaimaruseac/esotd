67e2e50ecc37c8661a55696fa881b4c1f47ab93e
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-06-02 21:54:39 -0700

    Parse arguments except those needed for output

--- exe/Args.hs
+++ exe/Args.hs
@@ -5,7 +5,59 @@ Description : Command line argument parsing.
 Functions and utilities to parse the arguments from the command line.
  -}
 
-module Args (parseArgs) where
+module Args
+  ( -- * parse the command line arguments
+    parseArgs
+    -- * structure representing the arguments
+  , Args(..)
+  ) where
 
-parseArgs :: IO Int -- TODO
-parseArgs = undefined
+import Options.Applicative
+
+data Args = Args
+  { coeffs :: [Int] -- ^ coefficients to generate the sum
+  , start  :: Int   -- ^ from where to start the sum
+  , end    :: Int   -- ^ where to end the summation
+  -- TODO: image related arguments
+  } deriving Show
+
+parseArgs :: IO Args
+parseArgs = execParser $ info (helper <*> parseArgsInternal) buildMod
+
+buildMod :: InfoMod a
+buildMod = mconcat
+  [ fullDesc
+  , header "Exponential sum of the day"
+  ]
+
+parseArgsInternal :: Parser Args
+parseArgsInternal = Args
+  <$> parseCoefs
+  <*> parseStart
+  <*> parseEnd
+
+parseCoefs :: Parser [Int]
+parseCoefs = many $ argument auto $ mconcat
+  [ metavar "COEFFICIENT"
+  , help "Coefficient of the exponential argument"
+  ]
+
+parseStart :: Parser Int
+parseStart = option auto $ mconcat
+  [ long "start"
+  , short 'a'
+  , metavar "INDEX"
+  , help "Start summing from INDEX"
+  , value 0
+  , showDefault
+  ]
+
+parseEnd :: Parser Int
+parseEnd = option auto $ mconcat
+  [ long "end"
+  , short 'b'
+  , metavar "INDEX"
+  , help "Sum only until INDEX"
+  , value 10000
+  , showDefault
+  ]
