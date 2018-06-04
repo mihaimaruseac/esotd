03d093d8b8fd550c8e79d3cbbf50506bb8c950c4
Mihai Maruseac <mihai.maruseac@gmail.com>
2018-06-03 22:26:25 -0700

    Skeleton for the plotting

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE RecordWildCards #-}
 
+import Data.Complex (realPart, imagPart)
+
 import Args (parseArgs, Args(..))
 import ExponentialSum (expSums)
+import Plot (display)
 
 main :: IO ()
 main = do
   Args{..} <- parseArgs
-  print $ take (end - start) $ drop start $ expSums $ map fromIntegral coeffs
+  display $ select start end $ getPoints coeffs
+  where
+    getPoints = map toCoords . expSums . map fromIntegral
+    select st en = take (en - st) . drop st
+    toCoords z = (realPart z, imagPart z)
---
+++ exe/Plot.hs
@@ -0,0 +1,11 @@
+{-|
+Module      : Plot
+Description : Plot the new image
+
+Plots the result of the exponential sum of the day.
+ -}
+
+module Plot (display) where
+
+display :: [(Double, Double)] -> IO ()
+display = undefined
