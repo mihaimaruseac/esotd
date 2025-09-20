9aa91638ab657f71dc960916b1c489cb4a129352
Mihai Maruseac <mihai.maruseac@gmail.com>
2025-09-20 07:50:12 -0800

    Fix tests finally
    Signed-off-by: Mihai Maruseac <mihai.maruseac@gmail.com>

--- src/ExponentialSum.hs
+++ src/ExponentialSum.hs
@@ -34,11 +34,15 @@ import Data.Complex
 -- >>> :{
 -- import Test.Quickcheck
 -- quickCheck $ \x -> realArg [] x == 0
+-- :}
 --
 -- 2. If the index is 0, we output 0 regardless of the coefficients, provided
 -- at least one is non-zero
 --
---     prop> a == 0 || realArg (a:xs) 0 == 0
+-- >>> :{
+-- import Test.Quickcheck
+-- quickCheck $ \a -> a == 0 || realArg (a:xs) 0 == 0
+-- :}
 --
 -- There might be a few more properties but we can ignore them for now, as
 -- some might fail due to numerical inaccuracies. We are also sidestepping
@@ -62,8 +66,11 @@ realArg xs n = sum $ zipWith power xs [1..]
 --
 -- Also, we have the following properties:
 --
--- prop> x == 0 || expVal [] x == 1
--- prop> a >= 0 || expVal (a:xs) 0 == 1
+-- >>> :{
+-- import Test.Quickcheck
+-- quickCheck $ \x -> x == 0 || expVal [] x == 1
+-- quickCheck $ \x -> a >= 0 || expVal (a:xs) 0 == 1
+-- :}
 expVal :: [Double] -> Double -> Complex Double
 expVal xs n = exp $ 2 * pi * (0 :+ realArg xs n)
 
